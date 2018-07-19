948e621aa3bad43a56e182b88ff0fabb1b9b92dd
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-19 15:07:12 -0700

    tomato tomato

---
+++ Module05/Exercise02/Exercise02script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var txt;
+    var Height;
+    var Weight;
+    var personName;
+
+    var personName = prompt("Please enter your name", "Guy Fieri");
+    /* if (personName == null || person == "") {
+        txt = "I wouldn't give my name either";
+    } else {
+        txt = "Hello " + personName + "! How are you today?";
+    } */
+
+    var Height = prompt("Please enter your Height in Feet", "6\'4\"");
+    var Weight = prompt("Please enter your Weight in pounds", "206");
+
+    var BMI = Weight / (Height x Height) x 703
+    
+/*    document.getElementById("demo").innerHTML = txt;
+}*/
