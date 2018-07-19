a158a1561427dd0737978d03d1f18a58246aa13e
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-19 15:13:33 -0700

    fieri

--- Module05/Exercise02/Exercise02script.js
+++ Module05/Exercise02/Exercise02script.js
@@ -5,16 +5,16 @@ function myFunction() {
     var personName;
 
     var personName = prompt("Please enter your name", "Guy Fieri");
-    /* if (personName == null || person == "") {
+    if (personName == null || personName == "") {
         txt = "I wouldn't give my name either";
     } else {
         txt = "Hello " + personName + "! How are you today?";
-    } */
+    } 
 
-    var Height = prompt("Please enter your Height in Feet", "6\'4\"");
+/*    var Height = prompt("Please enter your Height in Feet", "6\'4\"");
     var Weight = prompt("Please enter your Weight in pounds", "206");
 
     var BMI = Weight / (Height x Height) x 703
-    
-/*    document.getElementById("demo").innerHTML = txt;
-}*/
+*/    
+    document.getElementById("demo").innerHTML = txt;
+}
