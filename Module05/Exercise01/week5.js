f436351b1313dd1d716639ab948e9d5f410b339c
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-19 13:52:12 -0700

    so close!

--- Module05/Exercise01/week5.js
+++ Module05/Exercise01/week5.js
@@ -1,5 +1,19 @@
 var firstName = prompt("Please enter your first name"); var secondName = prompt("Please enter your last name");
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
+if (hourNow > 18) {
+    greeting = 'Good evening';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'Good morning';
+} else {
+    greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '%nbsp;  ' + firstName + '</h3>');
 
 /* alert: Welcome User!
 var firstname;
@@ -9,4 +23,21 @@ prompt:  Ask the user for the firstname and store it in variable firstname (var 
 prompt:  Ask the user for the second name and store it in a variable lastname (var lastname;)
 In the Message concatenate the firstname and lastname  (HINT:  Concatenation in Javascript is done by the + symbol).
 Display message by using: 
-document.write('<h3>' + message + '</h3>'); */
+document.write('<h3>' + message + '</h3>');
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
+*/
