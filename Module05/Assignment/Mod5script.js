bc9d4029150d26448a1a3f8f8b727fec3d86dcf2
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-20 00:37:23 -0700

    risky java

--- Module05/Assignment/Mod5script.js
+++ Module05/Assignment/Mod5script.js
@@ -1,16 +1,9 @@
 var table = prompt("what would you like to multiply by today?","3");    // Unit of table
 var operator = 'multiplication';                                        // Type of calculation
 var i = 1;                                                              // Set counter to 1
-var msg = '';                                                           // Message
+var msg = '<h2> + "Multiplication Table" + </h2> + <br>';                                                           // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
+if (operator === 'multiplication') {
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
