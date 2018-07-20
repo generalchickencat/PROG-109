6d014d7be272377056eaef8e6d8a274b991ac5b6
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-20 16:34:06 -0700

    final with comments

--- Module05/Assignment/Mod5script.js
+++ Module05/Assignment/Mod5script.js
@@ -4,15 +4,19 @@ operator was elliminated because we're only doing a multiplication table.
 i is the value 1, because we don't want to start with negative numbers or zero, that's messed up.
 msg is the header and the initiation point.
 */
-var table = prompt("what would you like to multiply by today?","3");    // Unit of table
-var i = 1;                                                              // Set counter to 1
-var msg = '<h2> "Multiplication Table"  </h2>';                         // Message
+var table = prompt("what would you like to multiply by today?",3);    // Unit of table
+
+/*I worked with Adam to sort out this code - not sure if it's the best solution, we found that if you typed any number of spaces it breaks the result - but with these two arguments prevent the user from typing in letters, or typing nothing at all, it doesn't stop them from putting a space there*/
+while (table === '' || isNaN(table)) { table = prompt("You must pick a number", 9)}
+
+var i = 1;                                       // Set counter to 1, so it doesn't start at 0
+var msg = '<h2> "Multiplication Table"  </h2>';  // Message plus message header
 //while function starts at 1 (from the variable i) and goes to 11, which gives it that little extra oomph you sometimes need.
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 
-// Write the message into the page
+// This writes the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
