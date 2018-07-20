49b48ee80afed30d1231c0ff69b5e46f8637b2b1
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-20 00:43:48 -0700

    i freaking did it.

--- Module05/Assignment/Mod5script.js
+++ Module05/Assignment/Mod5script.js
@@ -1,14 +1,17 @@
+/* first let's talk about our variables:
+table is the value we're multiplying by, so we prompt for that number.
+operator was elliminated because we're only doing a multiplication table.
+i is the value 1, because we don't want to start with negative numbers or zero, that's messed up.
+msg is the header and the initiation point.
+*/
 var table = prompt("what would you like to multiply by today?","3");    // Unit of table
-var operator = 'multiplication';                                        // Type of calculation
 var i = 1;                                                              // Set counter to 1
-var msg = '<h2> + "Multiplication Table" + </h2> + <br>';                                                           // Message
-
-if (operator === 'multiplication') {
+var msg = '<h2> "Multiplication Table"  </h2>';                         // Message
+//while function starts at 1 (from the variable i) and goes to 11, which gives it that little extra oomph you sometimes need.
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
