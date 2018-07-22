562155ce7884e8e26e93c221bb6cf12561ea3bca
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-21 19:20:49 -0700

    maybe?

--- Module06/Exercise01/scripts.js
+++ Module06/Exercise01/scripts.js
@@ -1,12 +1,26 @@
-var table = prompt("Enter a number between 1 and 10", 9);        // Unit of table
-var operator = prompt("Choose Add or Multiply", "Add");          // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+//Unit of Table
+var table
+var operator
 
-generateTable(table, operator)  
-function generateTable(base, operator) {
-    
-if (operator === 'addition') {
+
+function execute () {
+    //message
+    var msg = '';
+    table = prompt("Enter a number from 1-10", 5);
+if (!(table >= 1 && table <= 10)) {
+  table = prompt("Try again. A number. 1-10. You can do this.");
+} 
+if (!(table >= 1 && table <= 10)) {
+  alert("Wow, you failed those really basic instructions. Enjoy your garbage code!");
+}
+if (document.getElementById("add").checked) {
+    operator = "add"
+}
+
+//Set counter to 1
+var i = 1;
+
+if (operator === 'add') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
@@ -19,7 +33,16 @@ if (operator === 'addition') {
     i++;
   }
 }
-}
+
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
+
+//document.getElementById("demo").innerHTML = x;
+/*    // Type of calculation
+operator = function myFunction() {
+    var x = document.getElementById("myRadio").checked;
+    document.getElementById("demo").innerHTML = x;
+};
+*/
+}
