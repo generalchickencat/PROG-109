b80d9057a1944aa1a8a891b04e85406f78bb2399
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-21 18:19:18 -0700

    before the radio buttons

---
+++ Module06/Exercise01/scripts.js
@@ -0,0 +1,25 @@
+var table = prompt("Enter a number between 1 and 10", 9);        // Unit of table
+var operator = prompt("Choose Add or Multiply", "Add");          // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '';              // Message
+
+generateTable(table, operator)  
+function generateTable(base, operator) {
+    
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+}
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
