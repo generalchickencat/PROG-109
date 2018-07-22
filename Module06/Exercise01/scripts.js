f962cb5e2e8cc1674ea01b61ff7e750ed448b6ab
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-07-21 20:03:36 -0700

    take a break

--- Module06/Exercise01/scripts.js
+++ Module06/Exercise01/scripts.js
@@ -1,47 +1,54 @@
 //Unit of Table
 var table
 
+/* Pressing a button makes the magic happen: */
 function execute () {
-    //message
-    var msg = '';
-    var operator = '';
-    table = prompt("Enter a number from 1-10", 5);
-if (!(table >= 1 && table <= 10)) {
-  table = prompt("Try again. A number. 1-10. You can do this.");
-} 
-if (!(table >= 1 && table <= 10)) {
-  alert("Wow, you failed those really basic instructions. Enjoy your garbage code!");
-}
-if (document.getElementById("add").checked) {
-    operator = "add"
-}
-
-//Set counter to 1
-var i = 1;
-
-if (operator === 'add') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
+  /* Anytime the button is pressed, msg and operator are emptied out. */
+  var msg = '';
+  var operator = '';
+  /* A prompt requests a value. */
+  table = prompt("Enter a number from 1-10", 5);
+  /* The validity of the value is checked. */
+  if (!(table >= 1 && table <= 10)) {
+    table = prompt("Try again. A number. 1-10. You can do this.");
+  } 
+  if (!(table >= 1 && table <= 10)) {
+    alert("Wow, you failed those really basic instructions. Enjoy your garbage code!");
+    /* If they entered invalid values, we default to 1. */
+    table = 1;
   }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
+  /* If the addition radio was checked, we assign an add value to operator */
+  if (document.getElementById("add").checked) {
+    operator = "add";
   }
+
+  /* Set counter to 1 */
+  var i = 1;
+  
+  /* If operator is add, addition is run. Otherwise, it defaults to multiplication. */
+  if (operator === 'add') {
+    /* Do addition */
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
+    }
+  } else {
+    /* Do multiplication */
+    while (i < 11) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
+    }
+  }
+
+  /* Write the message into the page */
+  var el = document.getElementById('blackboard');
+  el.innerHTML = msg;
 }
-
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
-
-//document.getElementById("demo").innerHTML = x;
-/*    // Type of calculation
+  
+/* Trash below. */
+/* document.getElementById("demo").innerHTML = x;
 operator = function myFunction() {
     var x = document.getElementById("myRadio").checked;
     document.getElementById("demo").innerHTML = x;
 };
 */
-}
