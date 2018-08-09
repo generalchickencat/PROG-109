9c0b6b7be0289793566c359c3801840152bf39f6
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-08-09 16:01:24 -0700

    bug hunter

--- Module10/Mod10Artifact/Module10ArtifactScript.js
+++ Module10/Mod10Artifact/Module10ArtifactScript.js
@@ -1,44 +1,40 @@
-var myImages = ["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg","http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
+var myImages = ["http://graypawn.wdfiles.com/local--files/imagebin/cashewchicken.jpg", "http://graypawn.wdfiles.com/local--files/imagebin/PinneappleFriedRice.jpg","http://graypawn.wdfiles.com/local--files/imagebin/SesamyChicken.jpg"];
 
+//var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
 
-var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+var index=0; 
 
- var index=0; 
-
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= captionImages[index];
- document.getElementById("caption").textContent = captionImages[index]; 
-} 
+function updateImage(){
+    document.getElementById("slideshow").src = myImages[index];
+//    document.getElementById("slideshow").alt= captionImages[index];
+//    document.getElementById("caption").textContent = captionImages[index]; 
+    } 
 
 function next(){
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
+    if (myImages.length == index+1){
+    index=0;}
+    else
+    {index++;
+    updateImage();
+    }}
 
 function back(){
     if (index===0)
     index=myImages.length-1;
     else
     index--;
-    
     updateImage();
-   } 
+    }
    
-   var nextButton = document.getElementById("next"); 
-   var previousButton = document.getElementById("previous"); 
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
    
-   previousButton.addEventListener("click",back,false);
-   nextButton.addEventListener("click",next,false); 
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
 
 function autoSlide(){
     if (document.getElementById("auto").checked)
-     next(); 
+    next(); 
     }
-    
-    
-    setInterval(autoSlide,2000); // Next
-    Sea
+
+setInterval(autoSlide,2000); // Next
