34783f4d130fede95335e9f50d310213c3375f4b
Andrew Henderson <andrew.henderson@bellevuecollege.edu>
2018-08-14 22:17:55 -0700

    SlideshowImproved

--- The Final Project/DJNP/05artifacts/Mod10/Mod10Script.js
+++ The Final Project/DJNP/05artifacts/Mod10/Mod10Script.js
@@ -1,8 +1,8 @@
 
 
-var myImages = [""];
+var myImages = ["https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/audience-1850022_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/audience-1867714_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/audience-1868137_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/concert-2527495_1280.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/concert-336695_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/concert-768722_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/party-629241_1920.jpg","https://generalchickencat.github.io/PROG-109/The%20Final%20Project/DJNP/05artifacts/Mod10/SlideshowImages/smartphone-407108_1920.jpg"];
 
-var captionImages =["Cashew","Pinneapple","Sesamy","S&S","White Rice","General Tsos","BBQ"];
+var captionImages =["Black Tie Affair","Black Tie Affair","The Argent Circle Opener","SakuraCon","Sindarin Heights","Angie's Heart","Neon Limits Ain't All That Bad","White Noise"];
 
 var index=0; 
 
